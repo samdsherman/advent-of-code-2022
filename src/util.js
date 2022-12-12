458a2e28c142546ed51d0c973116ef233415136a
Sam Sherman <samdsherman@gmail.com>
2022-12-12 12:32:22 -0800

    3

--- src/util.js
+++ src/util.js
@@ -2,5 +2,8 @@ import fs from 'fs/promises'
 export const getInput = async (day) => {
   const file = await fs.open(`inputs/${day}.txt`)
   const input = await file.readFile()
+  file.close()
   return input.toString()
 }
+
+export const sum = (array) => array.reduce((s, next) => s + next)
